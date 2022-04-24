9ce2d9a1acfb2c61078e7dd7cff878620f16f921
kirill <kirill-esau@outlook.de>
2022-04-24 18:25:33 +0200

    Erstellung eines Gateways

---
+++ src/main/java/de/kirill/minervagateway/MinervaGatewayApplication.java
@@ -0,0 +1,13 @@
+package de.kirill.minervagateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinervaGatewayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MinervaGatewayApplication.class, args);
+  }
+
+}
---
+++ src/test/java/de/kirill/minervagateway/MinervaGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package de.kirill.minervagateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinervaGatewayApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
